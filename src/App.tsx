6f834162d4e12750ab2038e6fb0602a2fce6522d
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-04-29 11:49:01 +0100

    using HOCs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Message } from "./Message";
+import Message from "./Message";
+
+const initialState = {
+  name: "Manny",
+  message: "Typescript is cool!",
+};
+
+// do not use this in production. Define the type
+type State = Readonly<typeof initialState>;
+
 function App() {
   // let firstValue: number[] = [2, 3, 4];
   // let firstValue: Array<number> = [2, 3, 4];
@@ -10,6 +19,7 @@ function App() {
     first, 
     second,
   }*/
+  let state: State = initialState;
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +27,7 @@ function App() {
         <p>
           The value {aTuple[0]} is of {typeof aTuple[0]} type!
         </p>
-        <Message message="This is a quick message" />
+        <Message name={state.name} message="This is a quick message" />
       </header>
     </div>
   );
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,11 @@
-export function Message(props: { message: string }) {
-  return <p>{props.message}</p>;
-}
+import messageHoc from "./Hoc";
+
+const example = (props: any): any => (
+  <p>
+    {props.name}, {props.message}
+  </p>
+);
+
+const Message = messageHoc(example);
+
+export default Message;
